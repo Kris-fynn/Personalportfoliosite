2d1ae9234fff4aa66bacaee521747e3d08a4ae05
Kris-fynn <67267786+Kris-fynn@users.noreply.github.com>
2022-03-01 13:58:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+
+document.getElementById('paragraph1').innerHTML = " My name is Christopher Fynn I am based in Johannesburg. I am a UNISA final year student, studying National Diploma in electrical engineering: computer systems (NDEEN-COS) which is more versatile for me since we are migrating from third to forth industrial evolution. <br> <br>  The reason why I chose to join this boot camp, it is because I will be able to analyse problems, design solutions using a computer language and develop maintenance plans to implement it in software, after studying all the theory and completing all the practical activities in this bootcamp. If I finish this bootcamp, I will be able upskill this year.";
+
+
+
+document.getElementById('paragraph2').innerHTML = " <strong> Main skills </strong><br> N+ A+ C++ JAVASCRIPT HTML5 CSS3 PHP MYSQL ANDROID LINUX PLC(programmable logic control) MatLab
+";
+
+
+
+const projects = []
+
+projects[3] = {item: "<strong> Main skills </strong>"};
+projects[2] = {item: " Projects: "};
+projects[0] = {item: "Projects: "};
+projects[4] = {item: " Projects: "};
+console.log(projects);
+
+
+ 
+    
